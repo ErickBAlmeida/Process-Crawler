2f3e730addc1dc9fa69707fc960fff2e2d146a4c
ErickBAlmeida <hxcxalmeida@gmail.com>
2025-08-05 18:03:51 -0300

    feat: Adicionar funcionalidades de navegação pelo site

--- index.py
+++ index.py
@@ -21,32 +21,56 @@ class App:
 
         self.notifier = ToastNotifier()
 
-        # self.wb = load_workbook("RJ.xlsx")
-        # self.sheet = self.wb.active
-
-        #   Configurar Navegador
+        self.wb = load_workbook("SP.xlsx")
+        self.sheet = self.wb.active
 
         subprocess.Popen([
             os.getenv("CHROME_PATH"),
-            f"--remote-debugging-port={os.getenv("DEBUG_PORT")}",
-            f"--user-data-dir={os.getenv("USER_DATA_DIR")}"
+            f"--remote-debugging-port={os.getenv('DEBUG_PORT')}",
+            f"--user-data-dir={os.getenv('USER_DATA_DIR')}"
         ])
-
-        time.sleep(3)
 
         #   Configurações do Chrome para se conectar via DevTools
         options = Options()
-        options.debugger_address = f"127.0.0.1:{os.getenv("DEBUG_PORT")}"
+        options.debugger_address = f"127.0.0.1:{os.getenv('DEBUG_PORT')}"
         options.add_argument("--start-maximized")
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
         
         self.navegador = webdriver.Chrome(service=Service(), options=options)
         self.navegador.get(os.getenv("LINK"))
 
         self.run()
-    
+
+    def logar(self):
+        self.navegador.find_element(By.ID, "identificacao").click()
+
+        btnCertificado = WebDriverWait(self.navegador, 10).until(
+            EC.presence_of_element_located((By.ID, "linkAbaCertificado"))
+        )
+        btnCertificado.click()
+
+        time.sleep(3)
+        self.navegador.find_element(By.ID, "submitCertificado").click()
+
+    def navegar(self):
+        burguerBtn = WebDriverWait(self.navegador, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "header__navbar__menu-hamburger"))
+        )
+        burguerBtn.click()
+
+        time.sleep(1)
+        self.navegador.find_element(By.XPATH, '//*[@id="root"]/div/header/nav/aside[1]/div[1]/nav/ul/li[2]/button').click()
+        time.sleep(1)
+        self.navegador.find_element(By.XPATH, '//*[@id="root"]/div/header/nav/aside[1]/div[1]/nav/ul/li[2]/ul/li[1]/a').click()
+
+    def pesquisar(self):
+        self.navegador.find_element(By.ID, 'numeroDigitoAnoUnificado').send_keys("10329758420248260562") # 13 primeiros digitos
+        self.navegador.find_element(By.ID, 'botaoConsultarProcessos').click()
+        
+        
     def run(self):
-        pass
+        self.logar()
+        self.navegar()
+        self.pesquisar()
 
 try:
     app = App()
